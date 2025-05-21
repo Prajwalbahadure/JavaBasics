06fdb7a8959a51b2a26b07938fa44860c8c8cd4b
Prajwal <prajwalbahadure5@gmail.com>
2025-05-21 22:05:31 +0530

    first program

---
+++ MyFirstProgram.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class MyFirstProgram 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello java ");
+	    System.out.println("My name is prajwal ");
+		System.out.println("How can I Help You");
+	
+	}
+
+}
